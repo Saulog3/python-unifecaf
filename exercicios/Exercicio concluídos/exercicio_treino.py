93c800d495561e3930c25b8087095070f0402d40
Saulo Cunha <saulorg3@gmail.com>
2025-09-11 12:25:33 -0300

    Commit all pending changes

--- exercicios/Exercicio concluídos/exercicio_treino.py
+++ exercicios/Exercicio concluídos/exercicio_treino.py
@@ -1,9 +1,22 @@
-def numerospares(numero):
-    listacompares = []
-    for n in range(numero + 1):
-        if n % 2 == 0:
-            listacompares.append(n)
-    return listacompares
+# def numerospares(numero):
+#     listacompares = []
+#     for n in range(numero + 1):
+#         if n % 2 == 0:
+#             listacompares.append(n)
+#     return listacompares
 
-n = int(input('Numero: '))
-print(numerospares(n))
+# n = int(input('Numero: '))
+# print(numerospares(n))
+
+# ---------------------------------------- #
+# lista = [1,2,3,98,4,5,7,8,10,36,85,47,32]
+
+# def maior(lista):
+#     maior = 0
+#     for num in lista: 
+#         if num > maior:
+#             maior = num
+#     return maior
+# print(maior(lista))
+        
+# ---------------------------------------#
