ceeaf64979357bb00a34edcbef9c92e92b95c57e
Saulo Cunha <167145321+Saulog3@users.noreply.github.com>
2025-09-17 21:39:23 -0300

    Create a01ex02.py

---
+++ exercicios/Exercicio concluídos/aula01_conceitos_basicos/a01ex02.py
@@ -0,0 +1,18 @@
+# Busque no texto principal o valor digitado pelo usuário, 
+# ao final retorne True se o valor for encontrado ou False 
+# caso contrário. Deve ser analisado caractere por caractere. 
+# Exemplo de entrada e saída:
+#     Entrada do usuário: brasil
+#     Texto: no brasil existem muitos lugares bonitos
+#     Retorno experado: True
+
+def buscar_texto(texto: str, valor: str) -> bool:
+    n = len(texto)
+    m = len(valor)
+
+    for i in range(n - m + 1):
+        if texto[i:i+m] == valor:
+            return True
+    return False
+
+texto = ''
