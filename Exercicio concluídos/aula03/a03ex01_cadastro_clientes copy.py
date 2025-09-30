23a21cd934451d9ef780d780475484e18467d712
Saulo Cunha <saulorg3@gmail.com>
2025-09-30 19:12:38 -0300

    Update das aulas

--- Exercicio concluídos/aula03/a03ex01_cadastro_clientes copy.py
+++ Exercicio concluídos/aula03/a03ex01_cadastro_clientes copy.py
@@ -14,8 +14,55 @@ config = {
     "host": "localhost",   # ou IP do servidor
     "user": "root",        # ex: root
     "password": "amon1010",    # senha do MySQL
-    "database": "loja_escolar" # banco de dados já criado
+    "database": "cadastro_atividade" # banco de dados já criado
 }
+conn = mysql.connector.connect(**config)
+cursor = conn.cursor()
+try:
+    # Conectando ao MySQL
+    conn = mysql.connector.connect(**config)
+    cursor = conn.cursor()
+
+    print("Conexão realizada com sucesso!")
+
+
+    # Criando tabela de exemplo
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS clientes (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        nome VARCHAR(100),
+        idade int
+    )
+    """)
+
+    # Inserindo dados
+    # cursor.execute(
+    #     "INSERT INTO usuarios (nome, email) VALUES (%s, %s)", 
+    #     ("Ana", "ana@email.com")
+    # )
+    conn.commit()
+
+    # Consultando dados
+    # cursor.execute("SELECT * FROM clientes" )
+
+    # for row in cursor.fetchall():
+    #     print(row)
+
+
+
+    cursor.execute("SELECT * FROM usuarios" )
+    for row in cursor.fetchall():
+        print(row)
+
+        
+except mysql.connector.Error as err:
+    print(f"Erro: {err}")
+
+finally:
+    if conn.is_connected():
+        cursor.close()
+        conn.close()
+        print("Conexão encerrada.")
 
 
 clientes = []
