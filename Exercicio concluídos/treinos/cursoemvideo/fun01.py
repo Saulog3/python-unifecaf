6a2dd0fbc2395963d86231c7907cc60641cf86c2
Saulo Cunha <167145321+Saulog3@users.noreply.github.com>
2025-12-24 00:03:18 -0300

    Update fun01.py

--- Exercicio concluídos/treinos/cursoemvideo/fun01.py
+++ Exercicio concluídos/treinos/cursoemvideo/fun01.py
@@ -1,11 +1,6 @@
 def fun (lista):
-    pass
-
-lista = [1,2,3,4,5,6]
-dobra = []
-
-for i in lista:
-    d = i * 2
-    dobra.append(d)
-
-print(dobra)
+    dobra = []
+    for i in lista:
+        d = i * 2
+        dobra.append(d)
+    print(dobra)
