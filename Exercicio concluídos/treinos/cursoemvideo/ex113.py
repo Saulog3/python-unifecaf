843ae8f9fa19428ed59afd326b227fb1c4da0866
Saulo Cunha <saulorg3@gmail.com>
2026-01-11 12:21:12 -0300

    Create ex113.py

---
+++ Exercicio concluídos/treinos/cursoemvideo/ex113.py
@@ -0,0 +1,9 @@
+def leiaInt(n):
+    
+    while n != int:
+        
+        print('Digite um valor inteiro')
+    return n
+
+n = leiaInt('Digite um número: ')
+print(f'Voce acabou de digitar o número {n}')
