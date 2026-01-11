908e451577a7c353e52923ffddea3d40999a89f7
Saulo Cunha <167145321+Saulog3@users.noreply.github.com>
2026-01-11 13:01:45 -0300

    update

--- Exercicio concluídos/treinos/cursoemvideo/ex104.py
+++ Exercicio concluídos/treinos/cursoemvideo/ex104.py
@@ -1,9 +1,16 @@
-def leiaInt(n):
-    
-    while n != int:
-        
-        print('Digite um valor inteiro')
-    return n
+def leiaInt(mensagem):
+    naoErro = False
+    valor = 0
+    while True:
+        n = str(input(mensagem))
+        if n.isnumeric():
+            valor = int(n)
+            naoErro = True
+        else:
+            print('\033[0;31mErro! Digite um número válido!.\033[m')
+        if naoErro:
+            break
+    return valor
 
 n = leiaInt('Digite um número: ')
 print(f'Voce acabou de digitar o número {n}')
--- Exercicio concluídos/treinos/cursoemvideo/ex113.py
+++ Exercicio concluídos/treinos/cursoemvideo/ex113.py
@@ -1,9 +1,25 @@
-def leiaInt(n):
-    
-    while n != int:
+def leiaInt(mensagem):
+ 
+
+
+
+    while True:
+
+        try:
+            n = int(input(mensagem))
+
+        except (ValueError, TypeError):
+            print('\033[0;31mErro! Digite um número válido!.\033[m')
         
-        print('Digite um valor inteiro')
+           
+        except Exception as erro:
+            print(f'Erro de {erro.__class__}')
+
+        else:
+
+            break
     return n
+ 
 
 n = leiaInt('Digite um número: ')
 print(f'Voce acabou de digitar o número {n}')
